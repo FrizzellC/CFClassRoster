93b6de80692f0e9bf04d1b07feeaa04a93678389
Chantel <chantelfrizzell@hotmail.com>
2014-11-03 21:10:27 -0800

    first commit

--- HomeworkWeek1/ViewController.swift
+++ HomeworkWeek1/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class Person {
+    
+    var firstName = ""
+    var lastName = ""
+    var isStudent = true
+    
+    init(first : String, last : String, student : Bool) {
+        self.firstName = first
+        self.lastName = last
+        self.isStudent = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func fullName() -> String {
+        return "\(firstName) \(lastName)"
+       
     }
-
-
 }
 
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        println("view did load")
+    }
+    
+    var myPerson = Person(first: "Chantel", last: " Frizzell", student: true)
+    
+    @IBAction func printName(sender: UIButton) {
+        println(myPerson.fullName())
+        
+    }
+    
+}
